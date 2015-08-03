43cd95ea318de5c0e4c3661dd15890b57d17729c
hannelita <hannelita@gmail.com>
2015-08-03 14:06:28 +0200

    Adding required methods according to doc_manager specs

--- project/mongo_connector/doc_managers/neo4j_doc_manager.py
+++ project/mongo_connector/doc_managers/neo4j_doc_manager.py
@@ -10,13 +10,13 @@ import sys
 import mongo_connector.doc_managers as doc_manager
 import os.path as path, sys
 
-sys.stdout.write(path.join(path.dirname(doc_manager.__file__),'neo4j_doc_manager.py'))
+# sys.stdout.write(path.join(path.dirname(doc_manager.__file__),'neo4j_doc_manager.py'))
 
 from threading import Timer
 
 import bson.json_util
 
-from py2neo import Graph, exceptions as es_exceptions
+from py2neo import Graph
 
 from mongo_connector import errors
 from mongo_connector.compat import u
@@ -53,26 +53,32 @@ class DocManager(DocManagerBase):
     """Inserts a document into Neo4j."""
     index, doc_type = self._index_and_mapping(namespace)
   
-  # def bulk_upsert(self, docs, namespace, timestamp):
-  #   """Insert multiple documents into Neo4j."""
+  def bulk_upsert(self, docs, namespace, timestamp):
+    """Insert multiple documents into Neo4j."""
 
-  # def update(self, document_id, update_spec, namespace, timestamp):
+  def update(self, document_id, update_spec, namespace, timestamp):
+    return
 
-  # def remove(self, document_id, namespace, timestamp):
+  def remove(self, document_id, namespace, timestamp):
+    return
 
-  # def search(self, start_ts, end_ts):
+  def search(self, start_ts, end_ts):
+    return
 
-  # def commit(self):
+  def commit(self):
+    return
 
-  # def get_last_doc(self):
+  def get_last_doc(self):
+    return
 
-  # def handle_command(self, doc, namespace, timestamp):
+  def handle_command(self, doc, namespace, timestamp):
+    return
 
 
-  # def _index_and_mapping(self, namespace):
-  #     """Helper method for getting the index and type from a namespace."""
-  #     index, doc_type = namespace.split('.', 1)
-  #     return index.lower(), doc_type
+  def _index_and_mapping(self, namespace):
+      """Helper method for getting the index and type from a namespace."""
+      index, doc_type = namespace.split('.', 1)
+      return index.lower(), doc_type
 
 
 
